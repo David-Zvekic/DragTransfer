ba7e81d289b528a96e94ebb6e69d24119d34b500
David Zvekic <david.zvekic@gmail.com>
2021-03-12 15:13:40 -0500

    confirms that the target and source are the same type
    	 	// if both source and target have the same type then allow deleting original item.
    		// this is a safety check because some game systems may allow dropping on targets
    		// that don't actually allow the GM or player to see the inventory, making the item
    		// inaccessible.

--- DragTransfer.js
+++ DragTransfer.js
@@ -1,17 +1,23 @@
 // DragTransfer
 // (c) 2021 David Zvekic
-// permission granted to use and distribute as per LICENSE file
 
-Hooks.on('dropActorSheetData',(actor,sheet,source,user)=>{
+Hooks.on('dropActorSheetData',(target,sheet,dragSource,user)=>{
  
-  if(source.type=="Item" && source.actorId) {
-	if(!actor.data._id) {
-		console.warn('target actor has no ID?',actor);
-		return; //if target actor doesn't seem to have an id, then simply bail 
+  if(dragSource.type=="Item" && dragSource.actorId) {
+	if(!target.data._id) {
+		console.warn("Drag'n'Trasfer - target has no data._id?",target);
+		return; 
 	}
-	if(actor.data._id ==  source.actorId) return;  // if dropping item back onto the same actor, just bail out.
-      let sourceActor = game.actors.get(source.actorId);
-    if(sourceActor) sourceActor.deleteOwnedItem( source.data._id);
+	if(target.data._id ==  dragSource.actorId) return;  // ignore dropping on self
+      let sourceActor = game.actors.get(dragSource.actorId);
+      if(sourceActor) {
+	 	// if both source and target have the same type then allow deleting original item.
+		// this is a safety check because some game systems may allow dropping on targets
+		// that don't actually allow the GM or player to see the inventory, making the item
+		// inaccessible.
+		if (sourceActor.data.type==target.data.type) 
+	      sourceActor.deleteOwnedItem( dragSource.data._id);
+	  }
     }
 });
   
