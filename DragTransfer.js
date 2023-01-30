bcdbe1d74c712555d26a0e6ab4986735a46240e6
Brian Blonski <bblonski@hotmail.com>
2023-01-30 11:17:08 -0800

    update for foundryvtt v10

--- DragTransfer.js
+++ DragTransfer.js
@@ -6,13 +6,10 @@
 
 import {registerSettings} from './settings/settings.js';
 
-let dragTransfer= new Object();
+let dragTransfer = new Object();
 
-Hooks.once('init',()=>{
-	
+Hooks.once('init', () => {
     registerSettings();
-	
-  
 });
 
 
@@ -26,80 +23,71 @@ function isSuperset(set, subset) {
     return true
 }
 
-Hooks.on('dropActorSheetData',(dragTarget,sheet,dragSource,user)=>{
+Hooks.on('dropActorSheetData', (dragTarget, sheet, dragSource, user) => {
 
-    
-  function isAlt(){
-     // check if Alt and only Alt is being pressed during the drop event.
 
-      let downKeys;
-      let alt;
-  
-      if (typeof game.keyboard.downKeys !== 'undefined') {
-        // FoundyVTT v9+ compatible  
-        downKeys = game.keyboard.downKeys;
-        alt = new Set(['AltLeft']);
-      } else {
-        // FoundryVTT v7, v8 compatible  
-        downKeys = game.keyboard._downKeys;
-        alt = new Set(['Alt']);
-      }
+    function isAlt() {
+        // check if Alt and only Alt is being pressed during the drop event.
 
-     return (isSuperset(alt,downKeys) && isSuperset(downKeys,alt));
-  }
-  
-  if (isAlt()) return;  // ignore Drag'N'Transfer when Alt is pressed to drop.
-	
-	
-  if(dragSource.type=="Item" && dragSource.actorId) {
-	
-	  
-	if(!dragTarget.data._id) {
-		console.warn("Drag'n'Transfer - target has no data._id?",dragTarget);
-		return; 
-	}
-	if(dragTarget.data._id ==  dragSource.actorId) return;  // ignore dropping on self
-      let sourceActor = game.actors.get(dragSource.actorId);
-      if(sourceActor) {
-	 	// if both source and target have the same type then allow deleting original item.
-		// this is a safety check because some game systems may allow dropping on targets
-		// that don't actually allow the GM or player to see the inventory, making the item
-		// inaccessible.
-				    
-		
-		  function checkCompatable(a,b){
-			  console.info('DragNTransfer - Check Compatability: Dragging Item:"' + String(dragSource.data.type) + '" from sourceActor.data.type:"' +String(a) +'" to dragTarget.data.type:"'+String(b)+'".');
-			  if(a==b) return true;
-			  try {
-				 
-     			  const transferPairs = JSON.parse("{"+ game.settings.get('DragTransfer', 'actorTransferPairs') +"}");
-				
-	              if(transferPairs[a]==b) return true;
-			      if(transferPairs[b]==a) return true;
-				  
-			  }
-	          catch(err){
-				  console.error('DragTransfer: ',err.message);
-				  ui.notifications.error('DragTransfer: '+err.message);
-	  	      }
-   		      return false;
-		  };
-	
-		if ( checkCompatable(sourceActor.data.type,dragTarget.data.type) ) {
-			if(sourceActor.deleteEmbeddedDocuments!=undefined) {
-			   sourceActor.deleteEmbeddedDocuments( "Item",[dragSource.data._id]);
-		    } else 
-			{
-		       sourceActor.deleteOwnedItem( dragSource.data._id);
+        let downKeys;
+        let alt;
+
+        if (typeof game.keyboard.downKeys !== 'undefined') {
+            // FoundyVTT v9+ compatible  
+            downKeys = game.keyboard.downKeys;
+            alt = new Set(['AltLeft']);
+        } else {
+            // FoundryVTT v7, v8 compatible  
+            downKeys = game.keyboard._downKeys;
+            alt = new Set(['Alt']);
+        }
+
+        return (isSuperset(alt, downKeys) && isSuperset(downKeys, alt));
+    }
+
+    if (isAlt()) return; // ignore Drag'N'Transfer when Alt is pressed to drop.
+
+
+    if (dragSource.type == "Item" && dragSource.uuid) {
+
+
+        if (!dragTarget.data._id) {
+            console.warn("Drag'n'Transfer - target has no data._id?", dragTarget);
+            return;
+        }
+        if (dragTarget.data._id == dragSource.uuid) return; // ignore dropping on self
+        let sourceActor = fromUuidSync(dragSource.uuid).actor;
+        if (sourceActor) {
+            // if both source and target have the same type then allow deleting original item.
+            // this is a safety check because some game systems may allow dropping on targets
+            // that don't actually allow the GM or player to see the inventory, making the item
+            // inaccessible.
+
+
+            function checkCompatable(a, b) {
+                console.info('DragNTransfer - Check Compatability: Dragging Item:"' + String(dragSource.type) + '" from sourceActor.data.type:"' + String(a) + '" to dragTarget.data.type:"' + String(b) + '".');
+                if (a == b) return true;
+                try {
+
+                    const transferPairs = JSON.parse("{" + game.settings.get('DragTransfer', 'actorTransferPairs') + "}");
+
+                    if (transferPairs[a] == b) return true;
+                    if (transferPairs[b] == a) return true;
+
+                } catch (err) {
+                    console.error('DragTransfer: ', err.message);
+                    ui.notifications.error('DragTransfer: ' + err.message);
+                }
+                return false;
+            };
+
+            if (checkCompatable(sourceActor.data.type, dragTarget.data.type)) {
+                if (sourceActor.deleteEmbeddedDocuments != undefined) {
+                    sourceActor.deleteEmbeddedDocuments("Item", [fromUuidSync(dragSource.uuid).id]);
+                } else {
+                    sourceActor.deleteOwnedItem(dragSource.data._id);
+                }
             }
-	    }
-	  }
+        }
     }
 });
-  
-
-   
-
-
-
-	  
